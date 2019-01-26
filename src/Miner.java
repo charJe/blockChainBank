8ae497bcecbf618ca80399febabeef05976a87e4
charJe <charles.b.jackson@protonmail.com>
2019-01-26 14:39:37 -0600

    Added runnable thread to receive Trans

--- src/Miner.java
+++ src/Miner.java
@@ -1,15 +1,41 @@
 import java.net.*;
+/**
+ * to run on the miners' computers. Remotely stores a log of all transactions
+ */
 public class Miner{
-    Socket me;
+    private static Socket me;
 
     public static void main(String[] args){
+        me =new Socket(InetAddress.getLocalHost(), 80);
+	Runnable r = new Runnable(){
+	    public void run(){
+		while(true) receiveTran();
+	    }
+        };
+	Thread receive = new Thread(r);
+    }
     
+    /**
+     * Receives one transaction from the bank server
+     *  
+     */      
+    public static void receiveTran(){
+	
     }
 
-    /*
-     *
+     /**
+     * 
+     * 
      */      
     public static void sendBlock(){
-
+	
     }
+     /**
+     * 
+     *  
+     */      
+    public static void updateBlockChain(){
+	
+    }
+    
 }
