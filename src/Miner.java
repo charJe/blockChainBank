def5da6692815ef5d0c5e2d943d9dadf3de86981
charJe <charles.b.jackson@protonmail.com>
2019-01-26 13:48:05 -0600

    added class

--- src/Miner.java
+++ src/Miner.java
@@ -1,5 +1,15 @@
 import java.net.*;
+public class Miner{
+    Socket me;
 
-public static void main(String[] args){
+    public static void main(String[] args){
     
+    }
+
+    /*
+     *
+     */      
+    public static void sendBlock(){
+
+    }
 }
